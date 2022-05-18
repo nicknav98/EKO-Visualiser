7d81c5408d7e3be91096f33ca5e8d55934ddb03c
nicknav98 <nicholasnavarro98@gmail.com>
2022-05-18 10:21:54 +0300

    Added query functionality to visualise.py, so data is extracted directly from database. Works as intended, next steps are to add multiple y lines, and tag each y line with time stamp

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # NERC Scripts, Personal Repo
+
+## User warnings, potential points of improvement
+
+Visualise script uses psycopg to connect to postgres, pandas produces warning:
+
+Tag(development, 1.0)
+
+UserWarning: pandas only support SQLAlchemy connectable(engine/connection) ordatabase string URI or sqlite3 DBAPI2 connectionother DBAPI2 objects are not tested, please consider using SQLAlchemy
+  warnings.warn(
--- visualize.py
+++ visualize.py
@@ -3,18 +3,46 @@
 # Author: Nicholas Navarro
 # First Revision: 17.5.22
 
+#Config
+HOST = "192.168.0.122"
+PORT = "5432"
+
+
+
+import psycopg2
 import pandas as pd
 import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
 
-plt.rcParams["figure.figsize"] = [7.00, 3.50]
-plt.rcParams["figure.autolayout"] = True
+try:
+    connection = psycopg2.connect(user="postgres",
+                                  password="aurinko88",
+                                  host=HOST,
+                                  port=PORT,
+                                  database="RadianceDB")
+    # cursor = connection.cursor()
+    sql_select_Query = "select * from radiance"
 
-df = pd.read_csv("./output.csv")
+    # cursor.execute(sql_select_Query)
+    print("Successfully extracted data from DB\n")
+    SQL_Query = pd.read_sql_query(sql_select_Query,connection)
+    df = pd.DataFrame(SQL_Query, columns=['Wavelength(nm)', 'Timestamp', 'Irradiance(W/m2/um)'])
 
-df.plot(x='Wavelength(nm)', y='Irradiance(W/m2/um)')
 
-plt.show()
+    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    plt.rcParams["figure.autolayout"] = True
 
+
+    df.plot(x='Wavelength(nm)', y='Irradiance(W/m2/um)')
+
+    plt.show()
+
+except(Exception, psycopg2.Error) as Error:
+    print("Error while connecting: ", Error)
+
+finally:
+    if connection:
+        connection.close()
+        print("PostgreSQL connection is closed")
 
