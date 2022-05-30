10899d9d41284e41c94ee2b269e20ab6389615d8
nicknav98 <nicholasnavarro98@gmail.com>
2022-05-30 11:22:43 +0300

    previous push comments apply to this visualizePloty update

--- visualizePlotly.py
+++ visualizePlotly.py
@@ -4,7 +4,7 @@
 # First Revision: 19.5.22
 
 # Config for connectiing to database
-HOST = "192.168.0.122"
+HOST = "172.24.59.97"
 PORT = "5432"
 
 import psycopg2
@@ -19,27 +19,28 @@ try:
                                   database="RadianceDB")
     # sql query to gather data, executes query and parses the timestamp column as datetime
     sql_select_Query = "select * from radiance"
-    SQL_Query = pd.read_sql_query(sql_select_Query, connection, parse_dates=['Timestamp'])
+    SQL_Query = pd.read_sql_query(sql_select_Query, connection)
     print("Successfully extracted data from DB\n")
 
     # converts sql data to pandas dataframe for plotting
     df = pd.DataFrame(SQL_Query, columns=['Wavelength(nm)', 'Timestamp', 'Irradiance(W/m2/um)'])
 
-    # START -(LEGACY CODE, used for matplotlib) splits table based on datetime, comparison is done with previous
-    # split to get dataframe chunks with same time stamp
-    # df1 = df[df['Timestamp'] != df.iloc[0, 1]]
-    # df2 = df1[df1['Timestamp'] != df1.iloc[0, 1]]
-    # df3 = df2[df2['Timestamp'] != df2.iloc[0, 1]]
-    # END of splitting
-
     # Parameters for graph layout
-
     # START --- Graph Construction Get Wavelenth value as numpy array, flattens it as a normal array, converts it to
     # int, and assigns to x-xis in graph
     x_axis = df['Wavelength(nm)'].to_numpy(dtype='float')
     x_axisArray = x_axis.flatten()
     x_axis_rounded = [round(x) for x in x_axisArray]
-    fig = px.line(df, x=x_axis_rounded, y='Irradiance(W/m2/um)', color=df['Timestamp'].to_numpy(dtype='str'))
+    fig = px.line(df, x=x_axis_rounded, y='Irradiance(W/m2/um)', color=df['Timestamp'],#.to_numpy(dtype='str'),
+                  labels={'x': 'Wavelength(nm)'})
+    fig.update_layout(
+        title={
+            'text': "Irradiance Spectrum by Hour",
+            'x': 0.5,
+            'xanchor': 'right',
+            'yanchor': 'top'},
+        plot_bgcolor='black',
+        legend_font_size=30)
     fig.show()
 
     # END
